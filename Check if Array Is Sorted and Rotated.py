003713852ec1075a60cef0341bccfa3e0d7ecb80
Iftikar Rahaman <119650896+IFTI-KAR@users.noreply.github.com>
2024-02-16 01:34:07 +0600

    Create Check if Array Is Sorted and Rotated.py

---
+++ Check if Array Is Sorted and Rotated.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+class Solution(object):
+    def check(self, nums):
+        for i in range(len(nums)-1,-1,-1):
+            if i==0:
+                return 1
+            if nums[i]<nums[i-1]:
+                break
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                break
+        if i-j==0 or i-j==1 and nums[-1] <= nums[0]:
+            return 1
+        else:
+            return 0 
